a7d975bacbc2cda450fcba1af9e946522d7beff0
Triwith <triwith.m@ku.th>
2017-02-17 18:41:31 +0700

    Sent Lab04#1

---
+++ src/coinpurse/BankNote.java
@@ -0,0 +1,57 @@
+package coinpurse;
+
+public class BankNote implements Valuable {
+	private static long nextSerialNumber = 1000000;
+	private double value;
+	private String currency = "Baht";
+	private long serialNumber;
+
+	public BankNote(double value) {
+		this.value = value;
+		this.serialNumber=BankNote.nextSerialNumber;
+		BankNote.nextSerialNumber++;
+	}
+
+	public BankNote(double value, String currency) {
+		this.value=value;
+		this.currency=currency;
+		this.serialNumber=BankNote.nextSerialNumber;
+		BankNote.nextSerialNumber++;
+
+	}
+
+	@Override
+	public double getValue() {
+		return this.value;
+	}
+
+	@Override
+	public String getCurrency() {
+		// TODO Auto-generated method stub
+		return this.currency;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+		if (obj.getClass() != this.getClass()) {
+			return false;
+		}
+		BankNote other = (BankNote) obj;
+		if (this.currency != other.currency || this.value != other.value) {
+			return false;
+		}
+		return true;
+	}
+
+	public long getSerial() {
+		return this.serialNumber;
+	}
+
+	public String toString() {
+		return this.value + "-" + this.currency + " note " + this.serialNumber;
+	}
+
+}
---
+++ src/coinpurse/Valuable.java
@@ -0,0 +1,13 @@
+package coinpurse;
+
+import java.util.Comparator;
+
+public interface Valuable {
+	/**
+	 * Get the monetary value of this object, in its own currency.
+	 * @return the value of this object
+	 */
+	public double getValue();
+	public String getCurrency();
+
+}
